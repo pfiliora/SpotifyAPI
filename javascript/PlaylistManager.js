a7e384d75ec3c6fa850fbb7e3536e26dc5b799d0
Paul Filiora <paul.filiora@gmail.com>
2017-02-23 11:37:26 -0500

    styling and bg

---
+++ javascript/PlaylistManager.js
@@ -0,0 +1,52 @@
+/*******************
+    PLAYLIST MANAGEMENT
+********************/
+
+const PlaylistManager = {};
+
+PlaylistManager.tracks = [];
+
+PlaylistManager.currentSong = 0;
+
+PlaylistManager.addTrack = (track = reqParam()) => {
+    PlaylistManager.tracks.push(track);
+//	PlaylistManager.saveData(PlaylistManager.tracks)
+}; // PlaylistManager.addTrack
+
+PlaylistManager.removeById = (id) => {
+    for (let i = 0; i < PlaylistManager.tracks.length; i++) {
+        const track = PlaylistManager.tracks[i];
+        if (track.id === id) {
+            PlaylistManager.tracks.splice(i, 1);
+            break;
+        }
+    }
+}
+
+PlaylistManager.getNextSong = () => {
+    PlaylistManager.currentSong++;
+    const {tracks, currentSong} = PlaylistManager;
+
+    const len = tracks.length;
+    if (currentSong === len) {
+        PlaylistManager.currentSong = 0;
+    }
+
+    return tracks[PlaylistManager.currentSong].id;
+}
+
+//PlaylistManager.saveData = function( data ) {
+//	localStorage.setItem('data', JSON.stringify( data ) );
+//	console.log(data)
+//}
+//
+//PlaylistManager.loadData = function( data ) {
+//	localStorage.getItem('data', JSON.parse( data ) );
+//	console.log(data)
+//}
+//
+//window.onload = () => {
+//  console.log('happy me on page load');
+//	localStorage.getItem('data', JSON.parse( data ) );
+//  ;
+//};
